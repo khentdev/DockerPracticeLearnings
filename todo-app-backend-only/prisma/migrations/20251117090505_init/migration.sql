772addd80b024ebc1f0554d645009e94485e4895
Kenneth Cacho <khentplays@gmail.com>
2025-11-17 17:36:15 +0800

    feat(docker): add fully functional dev and prod containers and fix package.json files

--- todo-app-backend-only/prisma/migrations/20251117090505_init/migration.sql
+++ todo-app-backend-only/prisma/migrations/20251117090505_init/migration.sql
@@ -1,12 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Todos` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE "Todos";
-
 -- CreateTable
 CREATE TABLE "todos" (
     "id" UUID NOT NULL,
--- todo-app-backend-only/src/registerRoute.ts
+++ todo-app-backend-only/src/registerRoute.ts
@@ -1,9 +1,9 @@
 import { Hono } from 'hono';
 
-import todoRoutes from './route.js';
+import todoRoutes from './controller/routes.js';
 
 export const registerRoutes = (app: Hono) => {
-    app.get("/", (c) => c.redirect("/health-ping"));
+    app.get("/", (c) => c.redirect("/health-ping", 301));
     app.get("/health-ping", (c) => c.json({ status: "ok" }, 200));
     app.route("/api", todoRoutes)
 }
